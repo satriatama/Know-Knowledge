f51baf9e333e59ec706ef854765854e6416ea5b7
satriatama <satriatama121@gmail.com>
2021-11-30 11:36:55 +0700

    ads

---
+++ Wali Murid/scriptDaftarWaliMurid.js
@@ -0,0 +1,45 @@
+const username = document.querySelector("#un");
+const namaLengkap = document.querySelector("#nl");
+const NIK = document.querySelector("#nik");
+const password = document.querySelector("#pw");
+const email = document.querySelector("#em");
+const password2 = document.querySelector("#cpw");
+const daftarBtn = document.querySelector("#Daftar");
+const kodeMurid = document.querySelector("#km");
+
+//untuk memberikan enter ganti baris
+function changeLine(fromInput, toInput) {
+  fromInput.addEventListener("keypress", function onEvent(event) {
+    if (event.keyCode == 13) {
+      event.preventDefault();
+      toInput.focus();
+    }
+  });
+}
+changeLine(NIK, username);
+changeLine(username, namaLengkap);
+changeLine(namaLengkap, password);
+changeLine(password, email);
+changeLine(email, password2);
+changeLine(password2, kodeMurid);
+changeLine(kodeMurid, daftarBtn);
+
+function validatePassword() {
+  if (password.value != password2.value) {
+    password2.setCustomValidity("Passwords tidak sama");
+  } else {
+    password2.setCustomValidity("");
+  }
+}
+password.onchange = validatePassword;
+password2.onfocus = validatePassword;
+
+daftarBtn.addEventListener("click", function (event) {
+  if (NIK.value == "" || username.value == "" || namaLengkap.value == "" || password.value == "" || email.value == "" || password2.value == "") {
+    event.preventDefault();
+    alert("Semua harus diisi!");
+    return false;
+  } else {
+    return true;
+  }
+});
